69ebf245b13e2e59d6154a1812ec82e595512816
eblondel <emmanuel.blondel1@gmail.com>
2017-02-21 21:33:36 +0100

    attempt to work with docker/geoserver

--- tests/test-all.R
+++ tests/test-all.R
@@ -8,9 +8,10 @@ gsPwd <- "geoserver"
 gsLogger <- "DEBUG"
 gsman <- try(GSManager$new(gsUrl, gsUsr, gsPwd, gsLogger))
 
-if(class(gsman) == "try-error"){
-  cat("GeoServer test instance is not started. Skipping integration tests...\n")
-}else if(is(gsman, "GSManager")){
+
+if(is(gsman, "GSManager")){
   cat(sprintf("GeoServer test instance started at %s. Running integration tests...\n", gsUrl))
   test_check("geosapi")
+}else{
+  cat("GeoServer test instance is not started. Skipping integration tests...\n")
 }
