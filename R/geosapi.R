a8cf68e65bb850db3a2b4cf74a785d6392d0a599
eblondel <emmanuel.blondel1@gmail.com>
2017-02-22 09:33:37 +0100

    enhance package description + link to Geoserver REST API documentation

--- R/geosapi.R
+++ R/geosapi.R
@@ -4,7 +4,12 @@
 #' @docType package
 #' 
 #' @title Geoserver REST API R Interface
-#' @description geosapi provides an R interface to the GeoServer REST web-services API.
+#' @description Provides an R interface to the Geoserver REST API, allowing to 
+#' upload and publish data in a GeoServer web-application and expose data to OGC 
+#' Web-Services. The package currently supports all CRUD (Create,Read,Update,Delete) 
+#' operations on GeoServer workspaces, namespaces, datastores (stores of vector data), 
+#' featuretypes, layers, styles, as well as vector data upload operations. For 
+#' more information about the GeoServer REST API, see http://docs.geoserver.org/stable/en/user/rest/ .
 #' 
 #' @details
 #'  \tabular{ll}{
