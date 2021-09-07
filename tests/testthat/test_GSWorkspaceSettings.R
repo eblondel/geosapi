a2f297a6266ff101f32bfb0a58d927f509c6bfc1
eblondel <emmanuel.blondel1@gmail.com>
2021-09-07 21:57:59 +0200

    refine version-dependent tests

--- tests/testthat/test_GSWorkspaceSettings.R
+++ tests/testthat/test_GSWorkspaceSettings.R
@@ -9,32 +9,41 @@ require(testthat)
 context("GSWorkspaceSettings")
 
 gsman$createWorkspace("geosapi")
+minVersion <- "2.12"
 
 test_that("CREATE workspace settings",{
-  settings <- GSWorkspaceSettings$new()
-  created <- gsman$createWorkspaceSettings("geosapi", settings)
-  expect_true(created)
+  if(gsman$version$equalTo(minVersion)|gsman$version$greaterThan(minVersion)){
+    settings <- GSWorkspaceSettings$new()
+    created <- gsman$createWorkspaceSettings("geosapi", settings)
+    expect_true(created)
+  }
 })
 
 test_that("READ workspace settings",{
-  settings <- gsman$getWorkspaceSettings("geosapi")
-  expect_true(!is.null(settings))
+  if(gsman$version$equalTo(minVersion)|gsman$version$greaterThan(minVersion)){
+    settings <- gsman$getWorkspaceSettings("geosapi")
+    expect_true(!is.null(settings))
+  }
 })
 
 test_that("UPDATE workspace settings",{
-  settings <- GSWorkspaceSettings$new()
-  settings$setNumDecimals(4)
-  settings$setVerbose(TRUE)
-  updated <- gsman$updateWorkspaceSettings("geosapi", settings)
-  settings2 <- gsman$getWorkspaceSettings("geosapi")
-  expect_equal(settings2$numDecimals, 4)
-  expect_true(settings2$verbose)
-  expect_true(updated)
+  if(gsman$version$equalTo(minVersion)|gsman$version$greaterThan(minVersion)){
+    settings <- GSWorkspaceSettings$new()
+    settings$setNumDecimals(4)
+    settings$setVerbose(TRUE)
+    updated <- gsman$updateWorkspaceSettings("geosapi", settings)
+    settings2 <- gsman$getWorkspaceSettings("geosapi")
+    expect_equal(settings2$numDecimals, 4)
+    expect_true(settings2$verbose)
+    expect_true(updated)
+  }
 })
 
 test_that("DELETE workspace settings",{
-  deleted <- gsman$deleteWorkspaceSettings("geosapi")
-  expect_true(deleted)
+  if(gsman$version$equalTo(minVersion)|gsman$version$greaterThan(minVersion)){
+    deleted <- gsman$deleteWorkspaceSettings("geosapi")
+    expect_true(deleted)
+  }
 })
 
 gsman$deleteWorkspace("geosapi")
