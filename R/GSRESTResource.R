1131e8a6435929dec576f4be3bc2468317bb3024
eblondel <emmanuel.blondel1@gmail.com>
2024-01-28 07:34:59 +0100

    fix #68

--- R/GSRESTResource.R
+++ R/GSRESTResource.R
@@ -10,10 +10,10 @@
 #' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
 #'
 GSRESTResource <- R6Class("GSRESTResource",
-                       
+  private = list(
+    rootName = NA
+  ),
   public = list(
-    #'@field rootName root name
-    rootName = NA,
     
     #'@description Initializes an object of class \link{GSRESTResource}
     #'@param xml object of class \link{xml_node-class}
@@ -22,7 +22,7 @@ GSRESTResource <- R6Class("GSRESTResource",
       if(missing(rootName) | is.null(rootName)){
         stop("No root name specified for GSRESTResource")
       }
-      self$rootName = rootName
+      private$rootName = rootName
     },
     
     #'@description Decodes from XML. Abstract method to be implemented by sub-classes
@@ -35,7 +35,7 @@ GSRESTResource <- R6Class("GSRESTResource",
     #'@return an object of class \link{xml_node-class}
     encode = function(){
       #Generic XML encoder
-      rootXML <- xml2::xml_new_root(self$rootName)
+      rootXML <- xml2::xml_new_root(private$rootName)
       if(is(self, "GSPublishable")){
         xml2::xml_attrs(rootXML) <- c(type = self$attr_type)
       }
@@ -44,7 +44,7 @@ GSRESTResource <- R6Class("GSRESTResource",
       fields <- rev(names(self))
       fields <- fields[!sapply(fields, function(x){
         (class(self[[x]])[1] %in% c("environment", "function")) ||
-        (x %in% c("rootName", "full", "attr_type"))
+        (x %in% c("full", "attr_type"))
       })]
       
       if(is(self, "GSRESTEntrySet")){
@@ -120,6 +120,120 @@ GSRESTResource <- R6Class("GSRESTResource",
       }
       
       return(rootXML)
+    },
+    
+    #'@description Provides a custom print output (as tree) of the current class
+    #'@param ... args
+    #'@param depth class nesting depth
+    print = function(..., depth = 1){
+      #list of fields to encode as XML
+      fields <- rev(names(self))
+      
+      #fields
+      fields <- fields[!sapply(fields, function(x){
+        (class(self[[x]])[1] %in% c("environment", "function")) ||
+          (x %in% private$system_fields)
+      })]
+      
+      cat(crayon::white(paste0("<", crayon::underline(self$getClassName()), ">")))
+      
+      for(field in fields){
+        fieldObj <- self[[field]]
+        
+        print_attrs <- function(obj){
+          paste(
+            sapply(names(obj$attrs), function(attrName){
+              paste0( crayon::magenta(attrName,"=",sep=""), crayon::green(obj$attrs[[attrName]]))
+            }
+            ), 
+            collapse=",")
+        }
+        
+        #user values management
+        shift <- "...."
+        if(!is.null(fieldObj)){
+          if(is(fieldObj, "GSRESTResource")){
+            attrs_str <- ""
+            if(length(fieldObj$attrs)>0){
+              attrs <- print_attrs(fieldObj)
+              attrs_str <- paste0(" [",attrs,"] ")
+            }
+            cat(paste0("\n", paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field), " ", attrs_str))
+            fieldObj$print(depth = depth+1)
+          }else if(is(fieldObj, "list")){
+            if(is.null(names(fieldObj))){
+              for(item in fieldObj){
+                if(is(item, "GSRESTResource")){
+                  attrs_str <- ""
+                  if(length(item$attrs)>0){
+                    attrs <- print_attrs(item)
+                    attrs_str <- paste0(" [",attrs,"] ")
+                  }
+                  cat(paste0("\n", paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field), " ", attrs_str))
+                  item$print(depth = depth+1)
+                }else if(is(item, "matrix")){
+                  m <- paste(apply(item, 1L, function(x){
+                    x <- lapply(x, function(el){
+                      if(is.na(suppressWarnings(as.numeric(el))) & !all(sapply(item,class)=="character")){
+                        el <- paste0("\"",el,"\"")
+                      }else{
+                        if(!is.na(suppressWarnings(as.numeric(el)))){
+                          el <- as.numeric(el)
+                        }
+                      }
+                      return(el)
+                    })
+                    return(paste(x, collapse = " "))
+                  }), collapse = " ")
+                  cat(paste0("\n",paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field), ": ", crayon::bgWhite(m)))
+                }else{
+                  cat(paste0("\n", paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field), ": ", crayon::bgWhite(item)))
+                }
+              }
+            }else{
+              cat(paste0("\n", paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field)))
+              for(itemName in names(fieldObj)){
+                item = fieldObj[[itemName]]
+                if(is(item, "GSRESTResource")){
+                  attrs_str <- ""
+                  if(length(item$attrs)>0){
+                    attrs <- print_attrs(item)
+                    attrs_str <- paste0(" [",attrs,"] ")
+                  }
+                  cat(paste0("\n", paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field), " ", attrs_str))
+                  item$print(depth = depth+1)
+                }else{
+                  cat(paste0("\n", paste(rep(shift, depth+1), collapse=""),"|-- ", crayon::italic(itemName), ": ", crayon::bgWhite(fieldObj[[itemName]])))
+                }
+              }
+            }
+          }else if (is(fieldObj,"matrix")){
+            m <- paste(apply(fieldObj, 1L, function(x){
+              x <- lapply(x, function(el){
+                if(is.na(suppressWarnings(as.numeric(el)))& !all(sapply(fieldObj,class)=="character")){
+                  el <- paste0("\"",el,"\"")
+                }else{
+                  if(!is.na(suppressWarnings(as.numeric(el)))){
+                    el <- as.numeric(el)
+                  }
+                }
+                return(el)
+              })
+              return(paste(x, collapse = " "))
+            }), collapse = " ")
+            cat(paste0("\n",paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field), ": ", crayon::bgWhite(m)))
+          }else{
+            cat(paste0("\n",paste(rep(shift, depth), collapse=""),"|-- ", crayon::italic(field), ": ", crayon::bgWhite(fieldObj)))
+          }
+        }
+      }
+      invisible(self)
+    },
+    
+    #'@description Get class name
+    #'@return an object of class \code{character}
+    getClassName = function(){
+      return(class(self)[1])
     }
   )                     
 )
